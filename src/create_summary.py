d5fa799fb506f212f359e0fcb67665c1496439c1
Maarten van Sluijs <72663441+MaartenVanSluijs@users.noreply.github.com>
2022-12-09 14:51:16 +0100

    Data cleaning

--- src/create_summary.py
+++ src/create_summary.py
@@ -11,33 +11,50 @@ data['price'] = data['price'].astype('double')
 data['service fee'] = data['service fee'].astype('double')
 data['last review'] = data['last review'].astype('datetime64')
 
+# print(data.dtypes)
 
-print(len(data['price'][(data['price'] - data['price'].mean()).abs() > (3*data['price'].std())]))
+def getSummary(data):
+  summary = pd.DataFrame(columns=['Attribute', '#_Null', '#_Outlier', 'Unique_values', 'Most_frequent', 'Count_Most_Freq', 'Max', 'Min'])
+
+  for column in data:
+      nunique = data[column].nunique()
+      nnull = data[column].isnull().sum()
+      most_freq = data[column].value_counts().idxmax()
+      count_most_freq  = data[column].value_counts()[most_freq]
+      max = np.NaN
+      min = np.NaN
+      noutlier = np.NaN
+      try:
+          max = data[column].max()
+          min = data[column].min()
+          std = data[column].std()
+          mean = data[column].mean()
+          noutlier = len(data[column][(data[column] - mean).abs() > (3*std)])
+      except:
+          pass
+      summary.loc[len(summary.index)] = [column, nnull, noutlier, nunique, most_freq, count_most_freq,max,min]
+
+  print(summary)
+  summary.to_csv('data_summary.csv')
 
 
-print(data.dtypes)
+def dropOutliers(data):
+  columns = ['lat', 'long', 'Construction year', 'price', 'service fee', 'minimum nights',
+       'number of reviews','reviews per month',
+       'review rate number', 'calculated host listings count',
+       'availability 365']
+  for column in columns:
+    std = data[column].std()
+    mean = data[column].mean()
+    data = data[ (data[column] - mean).abs() < (3 * std)]
+  return data
 
+data.drop('license', axis=1, inplace=True)
+data.drop('house_rules', axis=1, inplace=True)
+data.dropna(inplace=True)
+data = dropOutliers(data)
+data = data[data['minimum nights']>0]
+data = data[data['availability 365']>0]
+getSummary(data)
 
-summary = pd.DataFrame(columns=['Attribute', '#_Null', '#_Outlier', 'Unique_values', 'Most_frequent', 'Count_Most_Freq', 'Max', 'Min'])
-
-for column in data:
-    nunique = data[column].nunique()
-    nnull = data[column].isnull().sum()
-    most_freq = data[column].value_counts().idxmax()
-    count_most_freq  = data[column].value_counts()[most_freq]
-    max = np.NaN
-    min = np.NaN
-    noutlier = np.NaN
-    try:
-        max = data[column].max()
-        min = data[column].min()
-        noutlier = len(data[column][(data[column] - data[column].mean()).abs() > (3*data[column].std())])
-    except:
-        pass
-    summary.loc[len(summary.index)] = [column, nnull, noutlier, nunique, most_freq, count_most_freq,max,min]
-
-
-
-print(summary)
-summary.to_csv('data_summary.csv')
-
+data.to_csv("airbnb_data.csv")
