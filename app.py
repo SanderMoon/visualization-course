f9b591d29b635384379f263dbe55ac9c075c8169
Maarten van Sluijs <72663441+MaartenVanSluijs@users.noreply.github.com>
2023-01-16 12:12:51 +0100

    Fixed df parameters for plots

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from jbi100_app.views.menu import make_menu_layout
 from jbi100_app.views.scatterplot import Scatterplot
 from jbi100_app.views.splom import Splom
 from jbi100_app.views.map import Map
+import jbi100_app.data as data
 
 from dash import html
 import plotly.express as px
@@ -11,14 +12,14 @@ from dash.dependencies import Input, Output
 
 if __name__ == '__main__':
     # Create data
-    df = px.data.iris()
+    df_iris, df_air = data.get_data()
 
     # Instantiate custom views
-    scatterplot1 = Scatterplot("Scatterplot 1", 'sepal_length', 'sepal_width', df)
-    scatterplot2 = Scatterplot("Scatterplot 2", 'petal_length', 'petal_width', df)
+    scatterplot1 = Scatterplot("Scatterplot 1", 'sepal_length', 'sepal_width', df_iris)
+    scatterplot2 = Scatterplot("Scatterplot 2", 'petal_length', 'petal_width', df_iris)
     features = ["sepal_width", "sepal_length", "petal_width", "petal_length"]
-    splom = Splom("splom", features, df)
-    map = Map("map", df)
+    splom = Splom("splom", features, df_iris)
+    map = Map("map", df_air)
 
     app.layout = html.Div(
         id="app-container",
@@ -74,4 +75,4 @@ if __name__ == '__main__':
       return map.update()
 
 
-    app.run_server(debug=False, dev_tools_ui=True)
+    app.run_server(debug=True, dev_tools_ui=True)
