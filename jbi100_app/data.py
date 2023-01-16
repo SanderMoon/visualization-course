4b7629dfe7403e3ce07a41722a6f5469e61ed3eb
Maarten van Sluijs <72663441+MaartenVanSluijs@users.noreply.github.com>
2023-01-16 12:08:31 +0100

    Added airbnb data to get_data

--- jbi100_app/data.py
+++ jbi100_app/data.py
@@ -1,10 +1,12 @@
 import plotly.express as px
+import pandas as pd
 
 
 def get_data():
     # Read data
     df = px.data.iris()
+    df_air = pd.read_csv("data/airbnb_open_data.csv")
 
-    # Any further data preprocessing can go her
+    # Any further data preprocessing can go here
 
-    return df
+    return df, df_air
